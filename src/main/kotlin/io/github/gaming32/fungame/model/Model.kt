13fb3434ee8ba709ed78cd4ea9455fd4178e1f58
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-11-26 22:27:46 -0600

    Remove some old code

--- src/main/kotlin/io/github/gaming32/fungame/model/Model.kt
+++ src/main/kotlin/io/github/gaming32/fungame/model/Model.kt
@@ -3,7 +3,7 @@ package io.github.gaming32.fungame.model
 import io.github.gaming32.fungame.util.Drawable
 import io.github.gaming32.fungame.util.ModelBuilder
 import org.joml.Vector3f
-import org.lwjgl.opengl.GL11.*
+import org.lwjgl.opengl.GL11.GL_TRIANGLES
 import org.ode4j.ode.DTriMeshData
 import org.ode4j.ode.OdeHelper
 
@@ -17,8 +17,7 @@ data class Model(val tris: List<Tri>) : Drawable {
     data class Vertex(
         val position: Vector3f,
         val normal: Vector3f? = null,
-        val uv: UV? = null,
-        var collisionCache: Vector3f? = null
+        val uv: UV? = null
     ) : Drawable {
         override fun draw(builder: ModelBuilder) {
             uv?.draw(builder)
@@ -28,38 +27,23 @@ data class Model(val tris: List<Tri>) : Drawable {
     }
 
     data class Tri(val a: Vertex, val b: Vertex, val c: Vertex, val material: Material? = null) : Drawable {
-        fun draw(builder: ModelBuilder, hasCollision: Boolean) {
-            if (hasCollision) {
-                builder.disable(GL_TEXTURE_2D)
-                builder.color(0f, 0f, 1f, 1f)
-                builder.begin(GL_LINES)
-                builder.vertex(a.position)
-                builder.vertex(Vector3f(a.normal).add(a.position))
-                builder.draw()
-            } else {
-                material?.draw(builder)
-            }
+        override fun draw(builder: ModelBuilder) {
+            material?.draw(builder)
             builder.begin(GL_TRIANGLES)
             a.draw(builder)
             b.draw(builder)
             c.draw(builder)
             builder.draw()
         }
-
-        override fun draw(builder: ModelBuilder) = draw(builder, false)
     }
 
-    fun draw(builder: ModelBuilder, collisions: List<Tri>) {
+    override fun draw(builder: ModelBuilder) {
         tris.forEach {
-            it.draw(builder, it in collisions)
+            it.draw(builder)
         }
     }
 
-    override fun draw(builder: ModelBuilder) = draw(builder, listOf())
-
-    fun toTriMeshData(
-        data: DTriMeshData = OdeHelper.createTriMeshData()
-    ) = data.also {
+    fun toTriMeshData(data: DTriMeshData = OdeHelper.createTriMeshData()) = data.also {
         val vertices = mutableMapOf<Vertex, Int>() // ORDERED!
         val indexData = IntArray(tris.size * 3)
         tris.forEachIndexed { index, tri ->
